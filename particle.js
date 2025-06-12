6125cb17cd295df27bcc02bb785e79e7e1eabab6
tumati <jgsoaresdias@gmail.com>
2025-06-12 15:35:11 -0300

    Add particle system

---
+++ particle.js
@@ -0,0 +1,57 @@
+class Particle {
+    constructor(particleSystem) {
+        this.particleSystem = particleSystem
+        this.cooldown = particleSystem.timeToLive
+        this.isDead = false
+        this.velocity = p5.Vector.random2D().setMag(random(particleSystem.minSpeed, particleSystem.maxSpeed))
+        this.position = particleSystem.position.copy()
+        this.gravity = particleSystem.gravity.copy()
+        this.color = particleSystem.startColor
+        this.startSize = random(particleSystem.minStartSize, particleSystem.maxStartSize)
+        this.size = this.startSize
+        this.endSize = random(particleSystem.minEndSize, particleSystem.maxEndSize)
+        this.startRotation = random(-PI/4, PI/4)
+        this.rotation = this.startRotation
+        this.angularSpeed = random(particleSystem.minAngularSpeed, particleSystem.maxAngularSpeed)
+    }
+
+    update(delta) {
+        if (this.cooldown <= 0) {
+            this._destroy()
+            return
+        } else {
+            this.cooldown -= delta
+        }
+        this._applyForce(p5.Vector.mult(this.gravity, delta))
+        this.position.add(p5.Vector.mult(this.velocity, delta))
+        this.rotation += this.angularSpeed * delta
+    }
+
+    show() {
+        let perc = 1-(this.cooldown/this.particleSystem.timeToLive)
+        this.color = lerpColor(this.particleSystem.startColor, this.particleSystem.endColor, perc)
+        this.size = lerp(this.startSize, this.endSize, perc)
+        push()
+        translate(this.position.x, this.position.y)
+        stroke(this.color)
+        fill(this.color)
+        rotate(this.rotation)
+        beginShape(TRIANGLES)
+        vertex(-this.size/2, this.size/2)
+        vertex(-this.size/2, -this.size/2)
+        vertex(this.size/2, 0)
+        endShape()
+        //circle(0, 0, this.size)
+        //rectMode(CENTER)
+        //rect(0, 0, this.size, this.size)
+        pop()
+    }
+
+    _destroy() {
+        this.isDead = true
+    }
+
+    _applyForce(force) {
+        this.velocity.add(force)
+    }
+}
---
+++ particleSystem.js
@@ -0,0 +1,61 @@
+class ParticleSystem {
+    constructor() {
+        this.particles = []
+        this.enabled = true
+        this.onShot = false
+        this.burstAmount = 100
+        this.interval = 2
+        this.timeToLive = 2
+        this.cooldown = 0
+        this.position = createVector(width/2, height/2)
+        this.minSpeed = 50
+        this.maxSpeed = 200
+        this.velocity
+        this.gravity = createVector(0, 100)
+        this.startColor = color(255, 0, 0, 255)
+        this.endColor = color(100, 100, 100, 0)
+        this.minStartSize = 5
+        this.maxStartSize = 10
+        this.minEndSize = 15
+        this.maxEndSize = 15
+        this.minAngularSpeed = -PI
+        this.maxAngularSpeed = PI
+    }
+
+    update(delta) {
+        console.log(this.particles.length)
+        if (this.cooldown > 0) {
+            this.cooldown -= delta
+        } else {
+            this.cooldown = this.interval
+            if (this.enabled){
+                this._addBurst()
+                if (this.onShot) this.enabled = false
+            }
+        }
+        let idx = 0
+        while (idx < this.particles.length) {
+            if (this.particles[idx].isDead) {
+                this.particles.splice(idx, 1)
+                continue
+            } else {
+                this.particles[idx].update(delta)
+            }
+            idx++
+        }
+    }
+
+    show() {
+        this.particles.forEach(particle => {
+            particle.show()
+        });
+    }
+
+    _addBurst() {
+        for (let i = 0; i < this.burstAmount; i++) {
+            let particle = new Particle(this)
+            this.particles.push(particle)
+        }
+    }
+
+}
